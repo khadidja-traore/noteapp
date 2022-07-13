17d1b035cd6f3111439b009a4ece69e8b109a4ad
khadidja <khadidja.te@gmail.com>
2022-07-13 13:04:48 +0200

    crée les composants topbar, sidebar, landingpage

---
+++ src/sidebar/sidebars.js
@@ -0,0 +1,8 @@
+/* global bootstrap: false */
+(() => {
+  
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
